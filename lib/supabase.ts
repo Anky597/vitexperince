abff473f001d2f9339658808064912d06ca28f0c
Aniket Nikam <125387241+Anky597@users.noreply.github.com>
2025-03-25 22:34:27 +0530

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = "https://yyxjgkkhdxnthcaulqto.supabase.co"
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl5eGpna2toZHhudGhjYXVscXRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY5NzA4NzAsImV4cCI6MjA1MjU0Njg3MH0.UODnWyQAr_krCzwMjNPdW5wcZbSRdblvCh-oRBChEJc"
-
+const supabaseUrl = "***"
+const supabaseAnonKey = "****"
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
